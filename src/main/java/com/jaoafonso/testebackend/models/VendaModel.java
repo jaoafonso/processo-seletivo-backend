d5f7a149568b7b1a84639450428d83513899a9c0
João Victor <facebookafonsolimajoaovictor@gmail.com>
2023-04-18 21:09:05 -0300

    VendaModel e VendedorModel adicionados

---
+++ src/main/java/com/jaoafonso/testebackend/models/VendaModel.java
@@ -0,0 +1,86 @@
+package com.jaoafonso.testebackend.models;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "TB_VENDA")
+public class VendaModel implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 6975616278908281179L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "venda_id")
+    private Long id;
+    @Column(name = "venda_data")
+    private Instant dataVenda;
+    @Column(name = "venda_valor", precision = 20, scale = 2)
+    private BigDecimal valor;
+    @ManyToOne
+    @JoinColumn(name = "vendedor_id")
+    private VendedorModel vendedor;
+
+    public VendaModel() {
+    }
+
+    public VendaModel(Long id, Instant dataVenda, BigDecimal valor, VendedorModel vendedor) {
+        this.id = id;
+        this.dataVenda = dataVenda;
+        this.valor = valor;
+        this.vendedor = vendedor;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getDataVenda() {
+        return dataVenda;
+    }
+
+    public void setDataVenda(Instant dataVenda) {
+        this.dataVenda = dataVenda;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public VendedorModel getVendedor() {
+        return vendedor;
+    }
+
+    public void setVendedor(VendedorModel vendedor) {
+        this.vendedor = vendedor;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        VendaModel venda = (VendaModel) o;
+
+        return Objects.equals(id, venda.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
---
+++ src/main/java/com/jaoafonso/testebackend/models/VendedorModel.java
@@ -0,0 +1,69 @@
+package com.jaoafonso.testebackend.models;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "TB_VENDEDOR")
+public class VendedorModel implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 8152777232931802857L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "vendedor_id")
+    private Long id;
+    @Column(name = "vendedor_nome")
+    private String nome;
+    @OneToMany(mappedBy = "vendedor")
+    private Set<VendaModel> vendas = new HashSet<>();
+
+    public VendedorModel() {
+    }
+
+    public VendedorModel(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<VendaModel> getVendas() {
+        return vendas;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        VendedorModel vendedor = (VendedorModel) o;
+
+        return Objects.equals(id, vendedor.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
